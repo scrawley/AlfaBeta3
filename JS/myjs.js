adbbce216279bfe6892d12842d250643df627a45
scrawley <scrawley@mica.edu>
2015-12-09 06:07:17 -0500

    Latest Update
    Fixing Image Quality and details of product pages.

--- JS/myjs.js
+++ JS/myjs.js
@@ -1,3 +1,43 @@
-//$(document).ready(function() {
-    //alert('hello');
-//});
+$(document).ready(function(){
+     $(".popup_zee").hide(); 
+    $( ".popup_elle" ).hide();
+    $( ".popup_vee" ).hide();
+    $( ".popup_eff" ).hide();
+    $( ".popup_kay" ).hide();
+ });
+
+$( ".close_window_link" ).click(function() {
+  $( ".popup_zee" ).hide();
+    $( ".popup_elle" ).hide();
+    $( ".popup_vee" ).hide();
+    $( ".popup_eff" ).hide();
+    $( ".popup_kay" ).hide();
+});
+
+$( ".addtocart" ).click(function() {
+  $( ".popup_zee" ).hide();
+    $( ".popup_elle" ).hide();
+    $( ".popup_vee" ).hide();
+    $( ".popup_eff" ).hide();
+    $( ".popup_kay" ).hide();
+});
+
+$( ".addtocart_zee" ).click(function() {
+  $( ".popup_zee" ).show();
+});
+
+$( ".addtocart_elle" ).click(function() {
+  $( ".popup_elle" ).show();
+});
+
+$( ".addtocart_vee" ).click(function() {
+  $( ".popup_vee" ).show();
+});
+
+$( ".addtocart_kay" ).click(function() {
+  $( ".popup_kay" ).show();
+});
+
+$( ".addtocart_eff" ).click(function() {
+  $( ".popup_eff" ).show();
+});
